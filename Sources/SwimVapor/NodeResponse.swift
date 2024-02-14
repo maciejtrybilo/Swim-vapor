b844c3fab37431814408c578800d9e84c041eb95
Maciej Trybilo <maciektr@gmail.com>
2024-02-14 22:47:51 +0100

    public

--- Sources/SwimVapor/NodeResponse.swift
+++ Sources/SwimVapor/NodeResponse.swift
@@ -5,12 +5,12 @@ import Vapor
 // This is especially important in HTMX where all responses from the
 // server should be HTML rather than the { error: true, reason: "..." }
 // JSON response that Vapor issues on throw Abort().
-struct NodeResponse: AsyncResponseEncodable {
+public struct NodeResponse: AsyncResponseEncodable {
     
     let node: Node
     let status: HTTPStatus
     
-    func encodeResponse(for request: Vapor.Request) async throws -> Vapor.Response {
+    public func encodeResponse(for request: Vapor.Request) async throws -> Vapor.Response {
         var headers = HTTPHeaders()
         headers.add(name: .contentType, value: "text/html")
         return .init(status: .ok, headers: headers, body: .init(string: node.render()))
